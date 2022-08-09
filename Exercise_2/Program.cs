96f39627d1bced68dcb27dfeeac9b511403f1be2
Artem Kandakov <Temofey1993@gmail.com>
2022-08-09 19:06:59 +0500

    Задача №2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное.
+Console.WriteLine("Введите первое число");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double y = Convert.ToDouble(Console.ReadLine());
+if(x>y) 
+{Console.WriteLine("Первое число больше второго, максимальное число = "+x);}
+else
+{Console.WriteLine("Второе число больше первого, максимальное число = "+y);}
